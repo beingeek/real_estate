30d961fa3f17b0526b6f36a2259965f5b4fb9eae
Furqan Asghar <furqan.790@outlook.com>
2023-02-06 16:53:49 +0500

    feat: overwrite payment plan prompt

--- real_estate/real_estate/doctype/property_booking_order/property_booking_order.js
+++ real_estate/real_estate/doctype/property_booking_order/property_booking_order.js
@@ -19,17 +19,24 @@ real_estate.PropertyBookingOrder = class PropertyBookingOrder extends frappe.ui.
 
 	payment_plan_template() {
 		let me = this;
-		return frappe.call({
-			method: "real_estate.real_estate.doctype.property_payment_plan_template.property_payment_plan_template.get_payment_plan",
-			args: {
-				plan_template: me.frm.doc.payment_plan_template
-			},
-			callback: function(r) {
-				if (r.message && !r.exc) {
-					return me.frm.set_value("payment_plan", r.message);
-				}
+		if (me.frm.doc.payment_plan_template) {
+			if (me.frm.doc.payment_plan) {
+				frappe.confirm('Are you sure you want to remake Payment Plan?',
+				() => {
+					return frappe.call({
+						method: "real_estate.real_estate.doctype.property_payment_plan_template.property_payment_plan_template.get_payment_plan",
+						args: {
+							plan_template: me.frm.doc.payment_plan_template
+						},
+						callback: function(r) {
+							if (r.message && !r.exc) {
+								return me.frm.set_value("payment_plan", r.message);
+							}
+						}
+					});
+				}, () => { })
 			}
-		});
+		}
 	}
 
 	property_unit() {
