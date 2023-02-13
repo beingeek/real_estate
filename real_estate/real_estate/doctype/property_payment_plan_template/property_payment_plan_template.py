cf8cd8df7140758353e78ef178e15b386820ecd3
Saif Ur Rehman <saif@paralogic.io>
2023-02-13 15:22:41 +0500

    refactor(Property Booking): Refactor

--- real_estate/real_estate/doctype/property_payment_plan_template/property_payment_plan_template.py
+++ real_estate/real_estate/doctype/property_payment_plan_template/property_payment_plan_template.py
@@ -6,7 +6,6 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils import cstr, getdate, flt, add_to_date
 import json
-from six import string_types
 
 
 class PropertyPaymentPlanTemplate(Document):
@@ -18,9 +17,9 @@ class PropertyPaymentPlanTemplate(Document):
 			if d.is_installment:
 				validate_payment_plan_row_installment(d)
 			else:
-				d.no_of_installment = ''
-				d.invoice_interval = ''
-				d.invoice_interval_count = ''
+				d.no_of_installments = 0
+				d.invoice_interval = None
+				d.invoice_interval_count = 0
 
 
 @frappe.whitelist()
@@ -36,7 +35,7 @@ def get_payment_plan(plan_template):
 		payment_plan.append({
 			'payment_plan_type': d.payment_plan_type,
 			'is_installment': d.is_installment,
-			'no_of_installment': d.no_of_installment,
+			'no_of_installments': d.no_of_installments,
 		})
 	return payment_plan
 
@@ -46,17 +45,17 @@ def validate_payment_plan_row_installment(payment_plan):
 	if not payment_plan:
 		return
 
-	if isinstance(payment_plan, string_types):
+	if isinstance(payment_plan, str):
 		payment_plan = frappe._dict(json.loads(payment_plan))
 
 	if payment_plan.is_installment:
 		message = ''
-		if not payment_plan.no_of_installment:
-			message += '<li>No of installment</li>'
+		if not payment_plan.no_of_installments:
+			message += '<li>No of Installments</li>'
 		if not payment_plan.invoice_interval:
-			message +=  '<li>Invoice interval</li>'
+			message +=  '<li>Invoice Interval</li>'
 		if not payment_plan.invoice_interval_count:
-			message += '<li>Invoice interval count</li>'
+			message += '<li>Invoice Interval Count</li>'
 
 		if message:
 			frappe.throw(_('Mandatory fields required in table <b>Payment Plan</b>, Row {0} <br><br><ul>{1}</ul>').format(payment_plan.idx, message))
