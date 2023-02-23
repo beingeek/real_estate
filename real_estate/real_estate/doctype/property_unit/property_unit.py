4fbb77ae7bae7b515ef3b0e95aed562229f4ea4f
Furqan Asghar <furqan.790@outlook.com>
2023-02-23 14:43:43 +0500

    fix: fixes and improvements

--- real_estate/real_estate/doctype/property_unit/property_unit.py
+++ real_estate/real_estate/doctype/property_unit/property_unit.py
@@ -13,14 +13,14 @@ class PropertyUnit(StatusUpdater):
 		self.validate_unit_template_project()
 
 	def validate_block_project(self):
-		block_project = frappe.get_cached_value('Block' ,self.block, 'project')
+		block_project = frappe.get_value('Block' ,self.block, 'project')
 		if self.project and self.project != block_project:
 			frappe.throw(_('Block is not linked with this Project'))
 
 	def validate_unit_template_project(self):
 		unit_template_project = frappe.get_cached_value('Unit Template' ,self.unit_template, 'project')
 		if self.project and self.project != unit_template_project:
-			frappe.throw(_('Unit Template is not linked with this Project'))
+			frappe.throw(_('Unit Template {0} does not belong to Project {1}').format(self.unit_template, self.project))
 
 	def set_status(self, update=False, status=None, update_modified=False):
 		bookings = frappe.get_all('Property Booking Order', {'property_unit': self.name, 'docstatus': 1})
@@ -34,12 +34,11 @@ class PropertyUnit(StatusUpdater):
 			self.db_set('booking_status', self.booking_status, update_modified=update_modified)
 
 	def set_unit_template_booked_by(self, update=False, status=None, update_modified=False):
-		bookings = frappe.get_all('Property Booking Order', {'property_unit': self.name, 'docstatus': 1})
+		bookings = frappe.get_all('Property Booking Order', fields=["customer"], filters={'property_unit': self.name, 'docstatus': 1})
 
-		if bookings and self.booking_status == 'Booked':
-			booked_by = frappe.get_value('Property Booking Order', bookings[0].name, 'customer')
-			self.booked_by = booked_by
-		elif self.booking_status == 'Available':
+		if bookings:
+			self.booked_by = bookings[0].customer
+		else:
 			self.booked_by = None
 
 		if update:
